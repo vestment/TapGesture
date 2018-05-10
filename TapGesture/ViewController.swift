301d8a94c168edc15b105ebe4e0616a4c874a1df
iMac <imac@madonna.local>
2018-05-10 17:48:16 +0200

    ahaahahahahaha`

--- TapGesture/ViewController.swift
+++ TapGesture/ViewController.swift
@@ -10,14 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        myImage.isUserInteractionEnabled = true
+        myImage.addGestureRecognizer(tapGestureRecognizer)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+        let tappedImage = tapGestureRecognizer.view as! UIImageView
+        
+        print("Done")
     }
 
 
